8ae61866dff3e5b012c54b6cd7a4d25dd1c3ea78
Alexander <92639298+Lykanthropie1@users.noreply.github.com>
2022-01-25 16:21:50 +0400

    task3 done

---
+++ app/addons/sd_departments/database/departments_static.sql
@@ -0,0 +1,6 @@
+REPLACE INTO ?:static_data (`param_id`, `param`, `param_2`, `param_3`, `param_4`, `param_5`, `param_6`, `class`, `section`, `status`, `position`, `parent_id`, `id_path`, `localization`, `company_id`) VALUES
+('192', 'departments.view', '', '', '', '1', 'N', 'ty-quick-menu-item__place-order', 'A', 'A', 80, 0, '192', '', 1);
+REPLACE INTO ?:static_data_descriptions (`param_id`, `lang_code`, `descr`) VALUES
+( '192', 'ru', 'Отделы');
+REPLACE INTO ?:static_data_descriptions (`param_id`, `lang_code`, `descr`) VALUES
+( '192', 'en', 'Departments');
